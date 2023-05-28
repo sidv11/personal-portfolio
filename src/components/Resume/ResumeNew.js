f086e1b6b357b35d2071d74e42c3aa4776986215
sidv11 <132430591+sidv11@users.noreply.github.com>
2023-05-29 00:34:04 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -8,9 +8,6 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "C:\Users\siddh\OneDrive\Desktop\final port\Siddhant Portfolio\src\Assets\Soumyajit_Behera-BIT_MESRA.pdf";
-
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
@@ -23,9 +20,22 @@ function ResumeNew() {
       <Container fluid className="resume-section">
         <Particle />
         <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
         </Row>
 
-       
+        <Row className="resume">
+          <Document file={pdf} className="d-flex justify-content-center">
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
+        </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
