c699f101130b40241431ca1f6b105c1bc0487afe
sidv11 <132430591+sidv11@users.noreply.github.com>
2023-05-29 00:14:15 +0530

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -6,6 +6,7 @@ import {
   SiJupyter,
   SiVirtualbox,
   SiR,
+  SiKalilinux
 } from "react-icons/si";
 
 function Toolstack() {
@@ -13,18 +14,39 @@ function Toolstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
+        <h6 className="name">
+          Visual Studio Code
+        </h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiLinux />
+        <h6 className="name">
+          Linux
+        </h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiJupyter />
+        <h6 className="name">
+          Jupyter Notebook
+        </h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVirtualbox />
+        <h6 className="name">
+          Virtual Box
+        </h6>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiR />
+        <h6 className="name">
+          R Studio
+        </h6>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiKalilinux />
+        <h6 className="name">
+          kali Linux
+        </h6>
       </Col>
     </Row>
   );
