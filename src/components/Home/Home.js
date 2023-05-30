1daaad1693cae6ed3ecfaf80a65ce64dc40241f3
sidv11 <132430591+sidv11@users.noreply.github.com>
2023-05-30 18:05:34 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
+import Home3 from "./Home3";
 import Type from "./Type";
 
 function Home() {
@@ -42,6 +43,7 @@ function Home() {
         </Container>
       </Container>
       <Home2 />
+      <Home3/>
     </section>
   );
 }
